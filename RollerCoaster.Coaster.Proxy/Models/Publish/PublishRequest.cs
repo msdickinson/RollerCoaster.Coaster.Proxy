4803e9a0cb9a3179bd4d78cd325d20cf2de13f27
Mark Dickinson <marksamdickinson@gmail.com>
2020-09-28 14:25:56 -0700

    update

---
+++ RollerCoaster.Coaster.Proxy/Models/Publish/PublishRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RollerCoaster.Coaster.Proxy.Models.PublishCoaster
+{
+    [ExcludeFromCodeCoverage]
+    public class PublishRequest
+    {
+        public string Name { get; set; }
+        public string Data { get; set; }
+    }
+}
---
+++ RollerCoaster.Coaster.Proxy/Models/Publish/PublishResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RollerCoaster.Coaster.Proxy.Models.PublishCoaster
+{
+    [ExcludeFromCodeCoverage]
+    public class PublishResponse
+    {
+        public int CoasterId { get; set; }
+        public string Token { get; set; }
+    }
+}
